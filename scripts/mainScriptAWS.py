8e22e28ef4bbdab90c1d30547025e8375be2fb20
Zalez95 <12982523+Zalez95@users.noreply.github.com>
2022-05-25 23:12:38 +0200

    Download multiple hashtags

--- scripts/mainScriptAWS.py
+++ scripts/mainScriptAWS.py
@@ -9,8 +9,8 @@ from fs import errors
 import sys
 
 
-class InstaProgressBar(ProgressBar):
-    def __init__(self, it, *args, **kwargs):
+class MyProgressBar(ProgressBar):
+    def __init__(self, it = None, *args, **kwargs):
         self.count = 0
         self.maximum = 0
         ProgressBar.__init__(self, it)
@@ -31,7 +31,7 @@ class InstaProgressBar(ProgressBar):
         perc = 0
         if self.maximum > 0:
             perc = 100.0 * self.count / float(self.maximum)
-        print("\r\033[KDownloaded:\t" + str(self.count) + "/" + str(self.maximum)
+        print("\r\033[KProgress:\t" + str(self.count) + "/" + str(self.maximum)
             + "\t" + '{:.3f}'.format(perc) + "%", end="")
 
 
@@ -45,16 +45,12 @@ class App:
     hashtag = ""
     instaMediaLimit = None
     s3fs = None
+    looterSession = None
+    rekognition = None
 
-    def run(self):
-        """ Downloads, stores and processes images with the requested
-            hashtag """
-        self.connectS3FS()
-        self.storeImages()
-        self.processImages()
-
-    def connectS3FS(self):
-        """ Connect to S3 storage """
+    def connect(self):
+        """ Connects to all the needed services """
+        # Connect to S3 storage
         try:
             print("Trying to connect to " + self.awsBucket + "...")
 
@@ -66,62 +62,91 @@ class App:
             )
 
             print("Successfully connected to " + self.awsBucket)
-            return True
         except Exception as e:
-            print("Failed to connect with AWS S3: " + str(e))
+            print("Failed to connect with " + self.awsBucket + ": " + str(e))
             return False
 
-    def storeImages(self):
-        """ Retrieve images and json from instagram and store them in S3 """
+        # Connect to Instagram
         try:
             print("Trying to connect to Instagram...")
 
-            looter = HashtagLooter(self.hashtag, dump_json=True, jobs=12)
+            looter = HashtagLooter(hashtag="", session=self.looterSession)
             looter.login(self.instaUser, self.instaPass)
 
-            print("Logged in? " + str(looter.logged_in()))
-            mediaCount = looter.download_pictures(
-                self.s3fs, media_count=self.instaMediaLimit, new_only=True,
-                dlpbar_cls=InstaProgressBar)
-
-            looter.logout()
-
-            print("Successfully downloaded from Instagram " + str(mediaCount) + " pictures")
-            return True
+            print("Successfully connected to Instagram")
         except Exception as e:
-            print("Failed to download all the pictures: " + str(e))
-            if (looter.logged_in()):
-                looter.logout()
+            print("Failed to connect to Instagram: " + str(e))
             return False
 
-    def processImages(self):
-        """ Process images with Rekognition and store them in S3 """
+        # Connect to Rekognition
         try:
             print("Trying to connect with Rekognition...")
 
-            rekognition_client = boto3.client(
+            self.rekognition = boto3.client(
                 service_name="rekognition",
                 region_name=self.awsRegion,
                 aws_access_key_id = self.awsAccessKeyId,
                 aws_secret_access_key = self.awsAccessKeySecret
             )
 
+            print("Successfully connected to Rekognition")
+        except Exception as e:
+            print("Failed to connect to Rekognition: " + str(e))
+            self.looter.logout()
+            return False
+
+        return True
+
+    def disconnect(self):
+        looter = HashtagLooter(hashtag="", session=self.looterSession)
+        looter.logout()
+
+        self.rekognition = None
+        self.looterSession = None
+        self.s3fs = None
+
+    def run(self):
+        """ Downloads, stores and processes images with the requested
+            hashtag """
+        return self.__storeImages() and self.__processImages()
+
+    def __storeImages(self):
+        """ Retrieve images and JSONs from Instagram and stores them in S3 """
+        try:
+            print("Trying to download images from Instagram...")
+
+            looter = HashtagLooter(self.hashtag, dump_json=True, jobs=12, session=self.looterSession)
+
+            print("Logged in? " + str(looter.logged_in()))
+            mediaCount = looter.download_pictures(
+                self.s3fs, media_count=self.instaMediaLimit, new_only=True,
+                dlpbar_cls=MyProgressBar)
+
+            print("Successfully downloaded from Instagram " + str(mediaCount) + " images")
+            return True
+        except Exception as e:
+            print("Failed to download all the images: " + str(e))
+            return False
+
+    def __processImages(self):
+        """ Process images with Rekognition and stores them as JSONs in S3 """
+        try:
+            print("Trying to process the images...")
+
             # Note: images that were download prior to the current run
             # but failed to retrieve its Rekognition JSON can also be
             # processed in this step
             filenames = self.s3fs.listdir("")
             imagesToProcess = list(filter(lambda x: x[-4:] == ".jpg" and not self.s3fs.exists(x[:-4] + "_rek.json"), filenames))
-            imageCount = 0
+            progressBar = MyProgressBar()
+            progressBar.set_maximum(len(imagesToProcess))
             imageSuccessCount = 0
 
             for image in imagesToProcess:
-                imageCount += 1
+                progressBar.update()
+                progressBar.printCurrent()
 
-                perc = 100.0 * imageCount / float(len(imagesToProcess))
-                print("\r\033[KRekognition:\t" + str(imageCount) + "/" + str(len(imagesToProcess))
-                    + "\t" + '{:.3f}'.format(perc) + "%", end="")
-
-                response = rekognition_client.detect_faces(
+                response = self.rekognition.detect_faces(
                     Image={"S3Object": {"Bucket": self.awsBucket, "Name": image}},
                     Attributes=["ALL", "DEFAULT"])
                 if response:
@@ -131,28 +156,40 @@ class App:
                 else:
                     print("\nFailed to process the image " + image)
 
-            print("\nSuccessfully proccessed " + str(imageSuccessCount) + " images")
+            progressBar.finish()
+            print("Successfully proccessed " + str(imageSuccessCount) + " images")
             return True
         except Exception as e:
-            print("\nFailed to process all the pictures: " + str(e))
+            print("\nFailed to process all the images: " + str(e))
             return False
 
 
 def main(argv):
     if len(argv) < 2:
-        print("Error: usage " + argv[0] + " <hashtag>")
+        print("Error: usage " + argv[0] + " <hashtag file (one per line)>")
         sys.exit()
 
-    app = App()
-    app.awsRegion = "eu-west-1"
-    app.awsBucket = "valltourisminstabucket"
-    app.instaUser = input("Instagram User: ")
-    app.instaPass = input("Instagram Password: ")
-    app.awsAccessKeyId = input("AWS Access Key ID: ")
-    app.awsAccessKeySecret = input("AWS Access Key Secret: ")
-    app.hashtag = argv[1]
-    app.instaMediaLimit = 50
-    app.run()
+    with open(argv[1], "r") as hashtagsFile:
+        lines = hashtagsFile.readlines()
+
+        app = App()
+        app.awsRegion = "eu-west-1"
+        app.awsBucket = "valltourisminstabucket"
+        app.instaUser = input("Instagram User: ")
+        app.instaPass = input("Instagram Password: ")
+        app.awsAccessKeyId = input("AWS Access Key ID: ")
+        app.awsAccessKeySecret = input("AWS Access Key Secret: ")
+        app.instaMediaLimit = 50
+
+        if app.connect():
+            for iLine in range(len(lines)):
+                hashtag = lines[iLine].strip()
+                print("=== Processing hashtag " + str(iLine + 1) + "/" + str(len(lines)) + ": #" + hashtag + " ===")
+                app.hashtag = hashtag
+                app.run()
+                print("=== Processed hashtag #" + hashtag + " ===")
+
+            app.disconnect()
 
 
 if __name__ == "__main__":
